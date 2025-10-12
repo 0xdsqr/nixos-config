219932cf93b44309c16e0d0d3f89fabf27f49713
DaveVED <me@dsqr.dev>
2025-10-12 13:49:40 -0500

    fix: use typo

--- users/sysdsqr/nixos.nix
+++ users/sysdsqr/nixos.nix
@@ -16,6 +16,8 @@
   # Since we're using fish as our shell
   programs.fish.enable = true;
 
+  users.groups.sysdsqr = {};
+  
   users.users.sysdsqr = {
     isNormalUser = true;
     group = "sysdsqr";
@@ -27,7 +29,7 @@
       "networkmanager"
     ];
     description = "its me dave";
-    initialPassword = "changeme";
+    initialPassword = "bingbong";
     #hashedPassword = "$6$eupftIGTj5TR3pZE$UKnZlQkUxWJpLUcYjVKtDP23b0p5c2tf66qyBZJZL7/ZrITmQ1epwjmQe0gKxzJuaUKZ8jlW/CyfrCcICIvN.0";
     #openssh.authorizedKeys.keys = [
     #  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfvrJELCv6dQp2VoceeVrtx1e0mnVo2FgNgu9o98BtF me@dsqr.dev"
