e1c08460d11ab8b0ca20aaecda0969a425370d65
DaveVED <me@dsqr.dev>
2025-10-30 21:37:09 -0500

    feat: dsqr more

--- flake.nix
+++ flake.nix
@@ -195,6 +195,12 @@
         homeManager = true;
       };
       
+      nixosConfigurations.gateway-vm-x86_64 = mkSystem "gateway-vm-x86_64" {
+        system = "x86_64-linux";
+        user = "sysdsqr";
+        darwin = false;
+        homeManager = true;
+      };
       # ------------------------------------------------------------
       # Dariwn Configurtations
       # ------------------------------------------------------------
