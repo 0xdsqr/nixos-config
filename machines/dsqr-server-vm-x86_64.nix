6a94a8d19552c2a2067120582055aec0fc3f4b8c
DaveVED <me@dsqr.dev>
2025-10-30 21:14:27 -0500

    feat: dsqr server

--- machines/dsqr-server-vm-x86_64.nix
+++ machines/dsqr-server-vm-x86_64.nix
@@ -28,7 +28,40 @@
 
   networking.hostName = "server";
   networking.domain = "dsqr.dev";
-  networking.firewall.allowedTCPPorts = [];
-  
+  networking.firewall.allowedTCPPorts = [
+    3000
+    3001
+    8080
+  ];
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."localhost" = {
+      listen = [
+        { addr = "0.0.0.0"; port = 8080; }
+      ];
+      locations."/api/" = {
+        proxyPass = "http://localhost:3001/api/";
+        proxyWebsockets = true;
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+      locations."/" = {
+        proxyPass = "http://localhost:3000/";
+        proxyWebsockets = true;
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+    };
+  };
+
   system.stateVersion = "25.05";
 }
