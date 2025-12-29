444e880670fdd155be84fe4fdc4abd8f64681fb6
DaveVED <me@dsqr.dev>
2025-12-29 12:25:02 -0600

    fix: add back in rust fs lol

--- machines/cellar-vm-x86_64.nix
+++ machines/cellar-vm-x86_64.nix
@@ -23,13 +23,13 @@
 
   # RustFS S3-compatible object storage
   # TODO: Enable once built/cached - takes forever to compile
-  # services.dsqr-rustfs = {
-  #   enable = true;
-  #   package = inputs.rustfs.packages.${pkgs.system}.default;
-  #   address = "0.0.0.0";
-  #   consoleAddress = "0.0.0.0";
+  services.dsqr-rustfs = {
+     enable = true;
+     package = inputs.rustfs.packages.${pkgs.system}.default;
+     address = "0.0.0.0";
+     consoleAddress = "0.0.0.0";
   #   # rootCredentialsFile = config.sops.secrets."rustfs/credentials".path;
-  # };
+   };
 
   # PostgreSQL database
   services.postgresql = {
