90fb36d2a59064e2f70a7a2ac23f7b05c6af92c6
DaveVED <me@dsqr.dev>
2025-12-31 11:24:48 -0600

    fix: needs this for startup?

--- machines/media-server-vm-x86_64.nix
+++ machines/media-server-vm-x86_64.nix
@@ -63,17 +63,11 @@
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
     preStart = ''
-      # Create config directory if it doesn't exist
-      mkdir -p /var/lib/qbittorrent/.config/qBittorrent
-
-      # Set a known password (adminadmin) in the config
-      cat > /var/lib/qbittorrent/.config/qBittorrent/qBittorrent.conf <<EOF
-      [Preferences]
-      WebUI\Username=admin
-      WebUI\Password_PBKDF2="@ByteArray(ARQ77eY1NUZaQsuDHbIMCA==:0WMRkYTUWVT9wVvdDtHAjU9b3b7uB8NR1Gur2hmQCvCDpm39Q+PsJRJPaCU51dEiz+dTzh4qbPsL8WkFljQYFQ==)"
-      EOF
-
-      chown -R qbittorrent:media /var/lib/qbittorrent/.config
+      # Remove password from config to force temporary password generation
+      CONFIG_FILE="/var/lib/qbittorrent/.config/qBittorrent/qBittorrent.conf"
+      if [ -f "$CONFIG_FILE" ]; then
+        sed -i '/WebUI\\Password/d' "$CONFIG_FILE"
+      fi
     '';
     serviceConfig = {
       ExecStart = "${pkgs.qbittorrent-nox}/bin/qbittorrent-nox --webui-port=8080 --confirm-legal-notice";
@@ -81,6 +75,8 @@
       Group = "media";
       StateDirectory = "qbittorrent";
       Restart = "on-failure";
+      StandardOutput = "journal";
+      StandardError = "journal";
     };
   };
 
