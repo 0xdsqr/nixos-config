81ba14d1a9b6312c74756537faa32c9586b7980b
DaveVED <me@dsqr.dev>
2026-01-02 19:25:23 -0600

    chore: a new host

--- machines/dsqr-server-vm-x86_64.nix
+++ machines/dsqr-server-vm-x86_64.nix
@@ -38,6 +38,24 @@
         '';
       };
     };
+   virtualHosts."admin.dsqr.dev" = {
+      listen = [
+        {
+          addr = "0.0.0.0";
+          port = 8080;
+        }
+      ];
+      locations."/" = {
+        proxyPass = "http://localhost:3001/";
+        proxyWebsockets = true;
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+    };
   };
 
   system.stateVersion = "25.05";
