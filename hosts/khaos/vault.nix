6987dab28276c7143d7b834905a29bc33a32b29a
0xdsqr <me@dsqr.dev>
2026-04-17 16:48:58 -0500

    feat: vault

---
+++ hosts/khaos/vault.nix
@@ -0,0 +1,16 @@
+{ ... }:
+{
+  services.vault = {
+    enable = true;
+    address = "0.0.0.0:8200";
+    storageBackend = "raft";
+    extraConfig = ''
+      ui = true
+      api_addr = "http://192.168.50.71:8200"
+      cluster_addr = "http://192.168.50.71:8201"
+      disable_mlock = true
+    '';
+  };
+
+  networking.firewall.allowedTCPPorts = [ 8200 ];
+}
