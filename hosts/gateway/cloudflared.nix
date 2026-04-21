a1ff36a7fdde39bc45825ebdce3ef1058858fe45
0xdsqr <me@dsqr.dev>
2026-04-20 22:51:09 -0500

    fet: more

--- hosts/gateway/cloudflared.nix
+++ hosts/gateway/cloudflared.nix
@@ -1,4 +1,7 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+let
+  tunnelId = "9c851b2c-8644-40a5-8cf4-ae7f63f4a20c";
+in
 {
   environment.systemPackages = with pkgs; [ cloudflared ];
 
@@ -10,102 +13,27 @@
   };
   users.groups.cloudflared = { };
 
-  age.secrets.cloudflaredCredentials = {
-    file = ./tunnel.credentials.age;
-    path = "/etc/cloudflared/credentials.json";
+  age.secrets.cloudflaredTunnelToken = {
+    file = ./cloudflared.token.age;
+    path = "/run/agenix/cloudflaredTunnelToken";
     owner = "cloudflared";
     group = "cloudflared";
     mode = "0400";
   };
 
-  services.cloudflared = {
-    enable = true;
-    tunnels = {
-      "af7ebddc-a096-441c-afe1-ab19bab3d8ab" = {
-        credentialsFile = config.age.secrets.cloudflaredCredentials.path;
-        default = "http_status:404";
-        ingress = {
-          "dsqr.dev" = {
-            service = "https://10.10.30.200";
-            originRequest = {
-              httpHostHeader = "dsqr.dev";
-              noTLSVerify = true;
-            };
-          };
-
-          "studio.dsqr.dev" = {
-            service = "https://10.10.30.200";
-            originRequest = {
-              httpHostHeader = "studio.dsqr.dev";
-              noTLSVerify = true;
-            };
-          };
-
-          "vault.dsqr.dev" = {
-            service = "http://10.10.30.107:8200";
-          };
-
-          "grafana.dsqr.dev" = {
-            service = "http://10.10.30.102:8000";
-          };
-          "prometheus.dsqr.dev" = {
-            service = "http://10.10.30.102:9090";
-          };
-
-          "rustfs.dsqr.dev" = {
-            service = "http://10.10.30.107:9001";
-          };
-          "s3.dsqr.dev" = {
-            service = "http://10.10.30.107:9000";
-          };
-          "cdn.dsqr.dev" = {
-            service = "http://10.10.30.107:9000";
-          };
-
-          "tastingswithtay.com" = {
-            service = "https://10.10.30.200";
-            originRequest = {
-              httpHostHeader = "tastingswithtay.com";
-              noTLSVerify = true;
-            };
-          };
-          "admin.tastingswithtay.com" = {
-            service = "https://10.10.30.200";
-            originRequest = {
-              httpHostHeader = "admin.tastingswithtay.com";
-              noTLSVerify = true;
-            };
-          };
-
-          "proxmox.dsqr.dev" = {
-            service = "https://10.10.10.109:8006";
-            originRequest = {
-              noTLSVerify = true;
-            };
-          };
-
-          "opnsense.dsqr.dev" = {
-            service = "https://10.10.10.1";
-            originRequest = {
-              noTLSVerify = true;
-            };
-          };
-
-          "tplink.dsqr.dev" = {
-            service = "https://10.10.10.2";
-            originRequest = {
-              noTLSVerify = true;
-            };
-          };
-
-          "r730xd-idrac.dsqr.dev" = {
-            service = "https://10.10.10.105";
-            originRequest = {
-              noTLSVerify = true;
-            };
-          };
-        };
-      };
+  systemd.services.cloudflared-managed-tunnel = {
+    description = "Cloudflare managed tunnel (${tunnelId})";
+    after = [ "network.target" "network-online.target" ];
+    wants = [ "network.target" "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      User = "cloudflared";
+      Group = "cloudflared";
+      Restart = "on-failure";
+      RestartSec = "5s";
+      WorkingDirectory = "/var/lib/cloudflared";
+      StateDirectory = "cloudflared";
+      ExecStart = "${pkgs.bash}/bin/bash -ec 'exec ${pkgs.cloudflared}/bin/cloudflared tunnel run --token \"$(cat ${config.age.secrets.cloudflaredTunnelToken.path})\"'";
     };
   };
 }
