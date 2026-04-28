70a38cdb200e1e544061b9fe5e35ff18c3d993ff
0xdsqr <me@dsqr.dev>
2026-04-28 13:06:26 -0500

    chore(secrets): rekey age files for current host identities

--- keys.nix
+++ keys.nix
@@ -6,9 +6,9 @@ let
   hosts = {
     srv-lx-beacon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEg0Wa1hKeMF7yipTmlxdC+ZIiY9sEvlG5wgqX76/64k root@srv-lx-beacon";
     srv-lx-gateway = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEu2uKz+OGZmCnECDZ3OMItW3rTInjNEFxgB4p5oDqxn root@srv-lx-gateway";
-    srv-lx-k8s-master-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBBozJ9oeMgl6KxcXZp+pcdEM9nfIiM+FVUixKxMeCia root@srv-lx-k8s-master-01";
-    srv-lx-k8s-node-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMMnEJyDzHVE/xRBU1tnwvTygI7jF2X9IMqqoUxfQPr1 root@srv-lx-k8s-node-01";
-    srv-lx-k8s-node-02 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC7GdKgSr1RZ1QQUuSWUiDc7DjdoSGkTODEinZV7HsFU root@srv-lx-k8s-node-02";
+    srv-lx-k8s-master-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBa0ivhIPpBU+lTUGz9/uK6eQd9rT9Y3UrSn55sMkmpe root@srv-lx-k8s-master-01";
+    srv-lx-k8s-node-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHB7+jrInU41DpDuFMzvyYDgpOkE2duOjn8Hg93wAQ9L root@srv-lx-k8s-node-01";
+    srv-lx-k8s-node-02 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILCoB+PBfoAJ60/vxDhBOaeUDIMWESfYyWgHgKNK9LGL root@srv-lx-k8s-node-02";
     srv-lx-khaos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO96/hopscQBRbeWkv6CCcCNpe/5lwYt13c3bEWBDkyD root@srv-lx-khaos";
     srv-lx-hoo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILtAEOpQXb6gSsRJ7mEQ7vZ+toKwo/YCynFRFo4/PUcs root@srv-lx-hoo";
     srv-lx-mailbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDrwjdEz7mbg7E7BO6FqNG4S6eAcx73ktPhniXDwTGBu root@srv-lx-mailbox";
