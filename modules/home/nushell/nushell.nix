2ac4841afea728b9945b104aa5d807effd7cdc22
0xdsqr <me@dsqr.dev>
2026-05-04 14:11:40 -0500

    feat: add nushell host extension hooks

--- modules/home/nushell/nushell.nix
+++ modules/home/nushell/nushell.nix
@@ -7,10 +7,17 @@
       ...
     }:
     let
+      inherit (lib.attrsets) recursiveUpdate;
       inherit (lib.modules) mkIf;
       inherit (lib.options) mkEnableOption mkOption;
       inherit (lib.strings) optionalString readFile;
-      inherit (lib.types) package;
+      inherit (lib.types)
+        anything
+        attrsOf
+        lines
+        package
+        str
+        ;
       cfg = config.dsqr.home.nu;
       defaultPackage =
         if pkgs.stdenv.isDarwin then
@@ -35,11 +42,37 @@
         integrations.enable = mkEnableOption "Nushell integrations" // {
           default = true;
         };
+
+        settings = mkOption {
+          type = attrsOf anything;
+          default = { };
+          description = "Additional Nushell settings recursively merged into the generated defaults.";
+        };
+
+        extraAliases = mkOption {
+          type = attrsOf str;
+          default = { };
+          description = "Extra Nushell aliases merged into the generated defaults.";
+        };
+
+        extraConfig = mkOption {
+          type = lines;
+          default = "";
+          description = "Additional Nushell config text appended after the generated defaults.";
+        };
+
+        zshPrelude = mkOption {
+          type = lines;
+          default = "";
+          description = "Extra zsh text prepended before the Darwin zsh-to-Nushell handoff.";
+        };
       };
 
       config = mkIf cfg.enable {
         home.file.".zshrc" = mkIf pkgs.stdenv.isDarwin {
           text = /* zsh */ ''
+            ${optionalString (cfg.zshPrelude != "") cfg.zshPrelude}
+
             # Ghostty launches zsh on Darwin; immediately hand off to Nushell
             # with the Home Manager-managed config so prompt/theme/integrations load.
             SHELL=${cfg.package}/bin/nu exec ${cfg.package}/bin/nu --config '${config.xdg.configHome}/nushell/config.nu'
@@ -49,7 +82,7 @@
         programs.nushell = {
           enable = true;
           inherit (cfg) package;
-          settings = {
+          settings = recursiveUpdate {
             show_banner = false;
             edit_mode = "vi";
             history = {
@@ -64,7 +97,7 @@
               partial = true;
               use_ls_colors = true;
             };
-          };
+          } cfg.settings;
           shellAliases = {
             v = "nvim";
             vim = "nvim";
@@ -73,9 +106,9 @@
             la = "ls -a";
             sl = "ls";
             tree = "eza --tree --git-ignore --group-directories-first";
-          };
-          extraConfig =
-            readFile ./nushell.config.nu
+          }
+          // cfg.extraAliases;
+          extraConfig = readFile ./nushell.config.nu
             + optionalString pkgs.stdenv.isDarwin /* nu */ ''
               # agenix only auto-discovers ~/.ssh/id_ed25519 and ~/.ssh/id_rsa,
               # so wrap it to use the homelab key by default on the Mac.
@@ -83,7 +116,8 @@
                 let key = ($env.HOME | path join ".ssh" "dsqr_homelab_ed25519")
                 ^agenix -i $key ...$args
               }
-            '';
+            ''
+            + optionalString (cfg.extraConfig != "") ("\n" + cfg.extraConfig);
         };
       };
     };
