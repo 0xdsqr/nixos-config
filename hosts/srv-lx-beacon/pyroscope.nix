98e7ad809e600fdfb7829023c91edbf94124c162
0xdsqr <me@dsqr.dev>
2026-07-29 13:49:14 -0500

    fix(observability): align Pyroscope ring transport

--- hosts/srv-lx-beacon/pyroscope.nix
+++ hosts/srv-lx-beacon/pyroscope.nix
@@ -33,7 +33,7 @@ in
       server = {
         http_listen_address = address;
         http_listen_port = port;
-        grpc_listen_address = "127.0.0.1";
+        grpc_listen_address = "0.0.0.0";
         grpc_listen_port = 9097;
       };
 
