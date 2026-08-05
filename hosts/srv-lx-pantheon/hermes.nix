58a39400bcf0c5a02340828b47ed36e05e269092
0xdsqr <me@dsqr.dev>
2026-08-05 09:02:23 -0500

    feat(hermes): enable hoo and vanalia

--- hosts/srv-lx-pantheon/hermes.nix
+++ hosts/srv-lx-pantheon/hermes.nix
@@ -3,11 +3,28 @@ _: {
   # environment files have been created. Each instance runs with isolated
   # memory, sessions, OAuth state, and resource limits.
   dsqr.nixos.hermes = {
-    enable = false;
+    enable = true;
 
     instances = {
-      hoo = { };
-      vanalia = { };
+      hoo = {
+        environmentAgeFile = ./hermes-hoo.env.age;
+        allowedUsers = [
+          "618575437995442197"
+          "980636531565949019"
+        ];
+      };
+
+      vanalia = {
+        environmentAgeFile = ./hermes-vanalia.env.age;
+        allowedUsers = [
+          "618575437995442197"
+          "980636531565949019"
+        ];
+        allowedChannels = [ "1465807038587076700" ];
+        freeResponseChannels = [ "1465807038587076700" ];
+        homeChannel = "1465807038587076700";
+        requireMention = false;
+      };
     };
   };
 }
